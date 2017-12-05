da5e7d5c1462f83b5a34d4a06a79c53851e9f494
Allysson Santos <aallysson0@gmail.com>
2017-12-05 21:34:08 -0200

    learning getDefaultProps

--- src/title.js
+++ src/title.js
@@ -3,9 +3,15 @@
 import React from 'react';
 
 const Title = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'Desconhecido',
+      lastname: 'Sem sobrenome'
+    };
+  },
   render: function () {
     return (
-      <h1>Ola {this.props.name}!</h1>
+      <h1>Ola {this.props.name + ' ' + this.props.lastname}!</h1>
     );
   }
 });
