a47e0c0bca7905f8136f5f7821cf3d178600464e
Allysson Santos <aallysson0@gmail.com>
2017-12-05 21:05:33 -0200

    Add standard lint and webpack validator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const validate = require('webpack-validator');
 
-module.exports = {
+module.exports = validate({
   devtool: 'source-map',
   entry: [
     'react-hot-loader/patch',
@@ -23,11 +24,18 @@ module.exports = {
   ],
 
   module: {
+    preLoaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      include: /src/,
+      loader: 'semistandard-loader'
+    }],
+
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
       include: /src/,
       loader: 'babel-loader'
-    }]
+    }],
   }
-}
+});
