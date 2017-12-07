c423d194ab09a6473148873abbc4f8913bef3f76
Allysson Santos <aallysson0@gmail.com>
2017-12-06 22:58:35 -0200

    Add key to every square component

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
 import React, { Component } from 'react';
-import Title from './title';
+import Square from './square';
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
-        <Title name='Allysson' />
+        {['blue', 'red', 'green'].map((color) => (
+          <Square key={color} color={color} />
+        ))}
       </div>
     );
   }
