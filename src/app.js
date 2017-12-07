ec3a6f3eb08f5d4d9a5b869ca7b13cc3933eacb5
Allysson Santos <aallysson0@gmail.com>
2017-12-06 23:02:57 -0200

    use index to fill key prop to avoid duplicated keys

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ class App extends Component {
   render () {
     return (
       <div className='container'>
-        {['blue', 'red', 'green'].map((color) => (
-          <Square key={color} color={color} />
+        {['blue', 'red', 'green'].map((color, index) => (
+          <Square key={index} color={color} />
         ))}
       </div>
     );
