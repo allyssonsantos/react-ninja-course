527c786df81e97d978bfecc1a464a65ea0f77b7c
Allysson Santos <aallysson0@gmail.com>
2017-12-06 22:47:19 -0200

    Change App component to class

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 import Title from './title';
 
-class App extends React.Component {
+class App extends Component {
   render () {
     return (
       <div className='container'>
