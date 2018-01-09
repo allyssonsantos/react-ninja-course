aa4367a37c02ef1e77805e42e274096191b1f6e6
Allysson dos Santos <aallysson0@gmail.com>
2018-01-09 20:34:32 -0200

    Forms - Input properties

--- src/app.js
+++ src/app.js
@@ -1,34 +1,25 @@
 'use strict';
 
 import React, { Component } from 'react';
-import Timer from './timer';
 
 class App extends Component {
   constructor () {
-    console.log('Constructor app');
     super();
     this.state = {
-      time: 0,
-      showTimer: true
+      value: 'Valor inicial'
     };
   }
-
-  componentWillMount () {
-    console.log('componentWillMount app');
-  }
-
-  componentDidMount () {
-    console.log('componentDidMount app');
-  }
-
   render () {
-    console.log('render app');
     return (
       <div>
-        <Timer time={this.state.time} />
-        <button onClick={() => {
-          this.setState({ time: this.state.time + 10 });
-        }}>Change props</button>
+        <form>
+          <input type='text' value={this.state.value} onChange={(e) => {
+            this.setState({
+              value: e.target.value
+            });
+          }} />
+          <input type='text' defaultValue='Valor defualtValue' />
+        </form>
       </div>
     );
   }
