8b9579ca30931aebbb5f436344601c6f33c1c355
Allysson dos Santos <aallysson0@gmail.com>
2018-01-21 12:15:42 -0200

    Removing unused files

--- module-01/github-app/src/app.js
+++ module-01/github-app/src/app.js
@@ -3,35 +3,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor () {
-    super();
-    this.state = {
-      checked: false,
-      showContent: false
-    };
-  }
+
 
   render () {
     return (
-      <div>
-        <label>
-          <input
-            type='checkbox'
-            checked={this.state.checked}
-            onChange={() => {
-              this.setState({
-                checked: !this.state.checked
-              }, () => {
-                this.setState({
-                  showContent: this.state.checked
-                });
-              });
-            }}
-          /> Mostrar conteúdo
-        </label>
-
-        {this.state.showContent && <div>Olha eu aqui</div>}
-      </div>
+      <div>My app</div>
     );
   }
 }
