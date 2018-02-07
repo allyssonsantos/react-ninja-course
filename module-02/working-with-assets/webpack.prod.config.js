44aad49ed2e2a5533cb99797463230da5dec352a
Allysson Santos <allysson.santos@catho.com>
2018-02-07 16:36:18 -0200

    Add webpack dashboard

--- module-02/working-with-assets/webpack.prod.config.js
+++ module-02/working-with-assets/webpack.prod.config.js
@@ -6,27 +6,31 @@ const validate = require('webpack-validator');
 
 const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = validate({
-  devtool: 'source-map',
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:4000',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, 'src', 'index')
-  ],
+  entry: path.join(__dirname, 'src', 'index'),
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name]-[hash].js',
-    publicPath: ''
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new DashboardPlugin(),
     new ExtractTextPlugin('[name]-[hash].css'),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"',
+      }
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
+
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+
     new HtmlPlugin({
       title: 'GitHub app',
       template: path.join(__dirname, 'src', 'html', 'template.html'),
@@ -50,7 +54,7 @@ module.exports = validate({
       test: /\.css$/,
       exclude: /node_modules/,
       include: /src/,
-      loaders: ['style', 'css']
+      loader: ExtractTextPlugin.extract('style', 'css')
     }],
   }
 });
