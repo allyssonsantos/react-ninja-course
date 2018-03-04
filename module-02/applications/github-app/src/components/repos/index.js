575b4890a10950886b74e2534f01a54efd1204df
Allysson dos Santos <aallysson0@gmail.com>
2018-03-03 21:01:46 -0300

    Change active pagination on click

--- module-02/applications/github-app/src/components/repos/index.js
+++ module-02/applications/github-app/src/components/repos/index.js
@@ -14,7 +14,7 @@ const Repos = ({ className, title, repos, handlePagination }) => (
         </li>
       ))}
     </ul>
-    <Pagination total={10} activePage={3} onClick={handlePagination} />
+    <Pagination total={10} activePage={repos.pagination.activePage} onClick={handlePagination} />
   </div>
 );
 
