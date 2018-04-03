92dd70ba5a9c43115ab8981230bf98605596d6a2
Allysson dos Santos <aallysson0@gmail.com>
2018-04-03 20:15:23 -0300

    Add and save files to state

--- module-02/applications/markdown-editor/src/app.js
+++ module-02/applications/markdown-editor/src/app.js
@@ -43,7 +43,13 @@ class App extends Component {
     this.handleSave = () => {
       if (this.state.isSaving) {
         localStorage.setItem(this.state.id, this.state.value);
-        this.setState({ isSaving: false });
+        this.setState({
+          isSaving: false,
+          files: {
+            ...this.state.files,
+            [this.state.id]: this.state.value
+          }
+        });
       }
     };
 
