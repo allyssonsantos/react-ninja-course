58351bdcb68a07769a2b0767bfc9274d2f3d75ff
Allysson Santos <aallysson0@gmail.com>
2018-04-28 14:25:57 -0300

    Add redux

--- module-03/hello-redux/main.js
+++ module-03/hello-redux/main.js
@@ -8,36 +8,8 @@ const counter = (state = 0, action) => {
   }
 };
 
-console.assert(
-  counter(0, {
-    type: 'INCREMENT',
-  }) === 1,
-);
+const { createStore } = Redux;
+const store = createStore(counter);
 
-console.assert(
-  counter(1, {
-    type: 'INCREMENT',
-  }) === 2,
-);
-
-console.assert(
-  counter(2, {
-    type: 'DECREMENT',
-  }) === 1,
-);
-
-console.assert(
-  counter(5, {
-    type: 'DECREMENT',
-  }) === 4,
-);
-
-console.assert(
-  counter(3, {
-    type: 'XXXX',
-  }) === 3,
-);
-
-console.assert(
-  counter(undefined, {}) === 0,
-);
+store.dispatch({ type: 'INCREMENT' });
+console.log(store.getState());
