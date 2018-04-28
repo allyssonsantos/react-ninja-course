f8ad4b9d30c0da3ab92e738ae8c995cd010b5e9e
Allysson Santos <aallysson0@gmail.com>
2018-04-28 14:01:27 -0300

    Examples

--- module-03/hello-redux/main.js
+++ module-03/hello-redux/main.js
@@ -1,2 +1,43 @@
 'use strict';
 
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1;
+    case 'DECREMENT': return state -1;
+    default: return state;
+  }
+};
+
+console.assert(
+  counter(0, {
+    type: 'INCREMENT',
+  }) === 1,
+);
+
+console.assert(
+  counter(1, {
+    type: 'INCREMENT',
+  }) === 2,
+);
+
+console.assert(
+  counter(2, {
+    type: 'DECREMENT',
+  }) === 1,
+);
+
+console.assert(
+  counter(5, {
+    type: 'DECREMENT',
+  }) === 4,
+);
+
+console.assert(
+  counter(3, {
+    type: 'XXXX',
+  }) === 3,
+);
+
+console.assert(
+  counter(undefined, {}) === 0,
+);
