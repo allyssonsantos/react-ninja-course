184b64efd51ac99d7d383d9b4279842311cf4cda
Allysson Santos <aallysson0@gmail.com>
2019-01-21 21:24:49 -0200

    manual implementation of combineReducers

--- module-03/todo-list/src/redux-flow/reducers/index.js
+++ module-03/todo-list/src/redux-flow/reducers/index.js
@@ -1,6 +1,15 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 import todos from './todos';
 import visibilityFilter from './visibility-filter';
+
+const combineReducers = reducers => (state = {}, action) =>
+  Object.keys(reducers).reduce(
+    (nextState, key) => ({
+      ...nextState,
+      [key]: reducers[key](state[key], action),
+    }),
+    {}
+  );
 
 export default combineReducers({
   todos,
