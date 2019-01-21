15d6f8614955e2177e1b86da1dec5f287d775469
Allysson Santos <aallysson0@gmail.com>
2019-01-21 19:30:09 -0200

    Finish visibility-filter tests and implementation

--- module-03/todo-list/src/redux-flow/reducers/visibility-filter/index.js
+++ module-03/todo-list/src/redux-flow/reducers/visibility-filter/index.js
@@ -1,5 +1,15 @@
-const visibilityFilter = () => {
-  return 'SHOW_ALL';
+import { SET_VISIBILITY_FILTER, SHOW_ALL } from './actions';
+
+export const initialState = SHOW_ALL;
+
+const visibilityFilter = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.payload.filter;
+
+    default:
+      return state;
+  }
 };
 
 export default visibilityFilter;
--- module-03/todo-list/src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
+++ module-03/todo-list/src/redux-flow/reducers/visibility-filter/visibility-filter.test.js
@@ -1,18 +1,65 @@
 import { expect } from 'chai';
 import deepFreeze from 'deep-freeze';
-import visibilityFilter from './index';
+import visibilityFilter, { initialState } from './index';
+import {
+  SET_VISIBILITY_FILTER,
+  SHOW_ALL,
+  SHOW_ACTIVE,
+  SHOW_COMPLETED,
+} from './actions';
 
 it('visibilityFilter should be a function', () => {
   expect(visibilityFilter).to.be.a('function');
 });
 
 it('should show all todos', () => {
-  const before = 'SHOW_COMPLETED';
+  const before = SHOW_COMPLETED;
   const action = deepFreeze({
-    type: 'SET_VISIBILITY_FILTER',
-    payload: { filter: 'SHOW_ALL' },
+    type: SET_VISIBILITY_FILTER,
+    payload: { filter: SHOW_ALL },
   });
-  const after = 'SHOW_ALL';
+  const after = SHOW_ALL;
+
+  expect(visibilityFilter(before, action)).to.be.equal(after);
+});
+
+it('should show just completed todos', () => {
+  const before = SHOW_ALL;
+  const action = deepFreeze({
+    type: SET_VISIBILITY_FILTER,
+    payload: { filter: SHOW_COMPLETED },
+  });
+  const after = SHOW_COMPLETED;
+
+  expect(visibilityFilter(before, action)).to.be.equal(after);
+});
+
+it('should show just active todos', () => {
+  const before = SHOW_ALL;
+  const action = deepFreeze({
+    type: SET_VISIBILITY_FILTER,
+    payload: { filter: SHOW_ACTIVE },
+  });
+  const after = SHOW_ACTIVE;
+
+  expect(visibilityFilter(before, action)).to.be.equal(after);
+});
+
+it('should return latest state when action is unkown', () => {
+  const before = SHOW_COMPLETED;
+  const action = deepFreeze({
+    type: 'UNKOWN',
+    payload: { filter: SHOW_ALL },
+  });
+  const after = SHOW_COMPLETED;
+
+  expect(visibilityFilter(before, action)).to.be.equal(after);
+});
+
+it('should return initial state when latest state is undefined', () => {
+  const before = undefined;
+  const action = deepFreeze({});
+  const after = initialState;
 
   expect(visibilityFilter(before, action)).to.be.equal(after);
 });
