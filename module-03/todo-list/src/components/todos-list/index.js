5087f8f599295ad27ea0b18b275f5fc30cb948f7
Allysson Santos <aallysson0@gmail.com>
2019-01-25 09:24:34 -0200

    Making filters work

--- module-03/todo-list/src/components/todos-list/index.js
+++ module-03/todo-list/src/components/todos-list/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleTodo } from 'reducers/todos/action-creators';
+import * as filterActions from 'reducers/visibility-filter/actions';
 
-const TodosList = ({ todos, handleToggleTodo }) => (
+const TodosList = ({ todos, activeFilter, handleToggleTodo }) => (
   <ul>
-    {todos.map(todo => (
+    {getVisibleTodos(todos, activeFilter).map(todo => (
       <li
         key={todo.id}
         onClick={handleToggleTodo(todo.id)}
@@ -16,8 +17,19 @@ const TodosList = ({ todos, handleToggleTodo }) => (
   </ul>
 );
 
+const getVisibleTodos = (todos, activeFilter) => {
+  const filterItems = {
+    [filterActions.SHOW_ALL]: todos,
+    [filterActions.SHOW_COMPLETED]: todos.filter(todo => todo.completed),
+    [filterActions.SHOW_ACTIVE]: todos.filter(todo => !todo.completed),
+  };
+
+  return filterItems[activeFilter];
+};
+
 const mapStateToProps = state => ({
   todos: state.todos,
+  activeFilter: state.visibilityFilter,
 });
 
 const mapDispatchToProps = dispatch => ({
