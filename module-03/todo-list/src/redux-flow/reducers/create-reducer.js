2dc9577d917435e2e1aea0c3c879cadd177b947e
Allysson Santos <aallysson0@gmail.com>
2019-01-25 09:51:08 -0200

    Creating createReducer function and using in reducers

---
+++ module-03/todo-list/src/redux-flow/reducers/create-reducer.js
@@ -0,0 +1,9 @@
+const createReducer = (initialState, handleActions) => (
+  state = initialState,
+  action
+) =>
+  handleActions.hasOwnProperty(action.type)
+    ? handleActions[action.type](state, action)
+    : state;
+
+export default createReducer;
--- module-03/todo-list/src/redux-flow/reducers/visibility-filter/index.js
+++ module-03/todo-list/src/redux-flow/reducers/visibility-filter/index.js
@@ -1,15 +1,10 @@
 import { SET_VISIBILITY_FILTER, SHOW_ALL } from './actions';
+import createReducer from '../create-reducer';
 
 export const initialState = SHOW_ALL;
 
-const visibilityFilter = (state = initialState, action) => {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.payload.filter;
-
-    default:
-      return state;
-  }
-};
+const visibilityFilter = createReducer(initialState, {
+  [SET_VISIBILITY_FILTER]: (state, action) => action.payload.filter,
+});
 
 export default visibilityFilter;
