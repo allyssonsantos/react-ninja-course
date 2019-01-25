3eba0313d161194dbea714e273c2ec46909aef63
Allysson Santos <aallysson0@gmail.com>
2019-01-25 09:38:15 -0200

    using createReducer

--- module-03/todo-list/src/redux-flow/reducers/todos/index.js
+++ module-03/todo-list/src/redux-flow/reducers/todos/index.js
@@ -4,30 +4,24 @@ import { ADD_TODO, TOGGLE_TODO } from './actions';
 
 export const initialState = [];
 
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        id: action.payload.id,
-        text: action.payload.text,
-        completed: false,
-      });
+const todos = createReducer(initialState, {
+  [ADD_TODO]: (state, action) =>
+    state.concat({
+      id: action.payload.id,
+      text: action.payload.text,
+      completed: false,
+    }),
+  [TOGGLE_TODO]: (state, action) =>
+    state.map(todo => {
+      if (todo.id !== action.payload.id) {
+        return todo;
+      }
 
-    case TOGGLE_TODO:
-      return state.map((todo) => {
-        if (todo.id !== action.payload.id) {
-          return todo;
-        }
-
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      });
-
-    default:
-      return state;
-  }
-};
+      return {
+        ...todo,
+        completed: !todo.completed,
+      };
+    }),
+});
 
 export default todos;
